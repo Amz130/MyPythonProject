87f7e55fe79e386b947b843630dabceff05987ad
Amz130 <Ami.Ige130@gmail.com>
2024-12-03 15:34:13 +0000

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -1,1 +1,14 @@
+import csv #so I can read the file with CSV reader
 
+#first test read the lines and print them
+def ReadTheFile(fileName):
+    with open(fileName, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            print(row)
+    csvfile.close()
+
+
+#For first test
+
+ReadTheFile('Data.csv')
